3602948696629ca0ce2935d260dfb9d9a56de5d5
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-12-15 11:44:54 +0900

    restructure tests

---
+++ README.md
@@ -0,0 +1,11 @@
+## program id (for test)
+82XBkYcPfaevmCNDJwV4EPcDrhWbvonN9iCUJaorfCRj
+
+keypair: tests/test_program_keypair.json
+
+🚨DO NOT use this address in production. This keypair file is registered in the public repo.
+
+## build
+cargo build-sbf
+
+## test
--- tests/run_test_validator.sh
+++ tests/run_test_validator.sh
@@ -3,5 +3,6 @@
 solana-test-validator \
   --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s test_validator_programs/metaplex.metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s.so \
   --bpf-program whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc test_validator_programs/whirlpool.whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc.so \
+  --bpf-program 82XBkYcPfaevmCNDJwV4EPcDrhWbvonN9iCUJaorfCRj ../target/deploy/onocoy_credit.so \
   --account-dir test_validator_accounts \
   --reset 
